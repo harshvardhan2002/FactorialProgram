f95bfee2996c902717a04aaa4d442da08add64ee
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-26 01:34:34 +0530

    Add files via upload

---
+++ FactorialProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorialCount = 1;
+            Console.WriteLine("Enter a number to check its Factorial: ");
+            int number=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorialCount *= i;
+            }
+            Console.WriteLine("Factorial of " + number + " is " +factorialCount);
+        }
+    }
+}
